6e3cc53f1b9d1358d02f70004c6dd53f22075d92
Loïc Lecuyer <titi.tata@free.fr>
2014-02-17 18:01:57 +0100

    Correction référence générator

--- Outils/Generator/Properties/AssemblyInfo.cs
+++ Outils/Generator/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Generator")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
